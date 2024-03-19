fbdf5e2506699961fcc4aece382037781c3a99f8
立瑜 <baredragon0@gmail.com>
2024-03-20 07:51:16 +0800

    Add files via upload

---
+++ js/滾動淡出.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+  var scrollPos = window.scrollY;
+  var windowHeight = window.innerHeight;
+  var heroVideo = document.getElementById('home');
+
+  if (scrollPos > windowHeight*0.9) {
+    heroVideo.style.opacity = '0';
+  } else {
+    heroVideo.style.opacity = '1';
+  }
+});
