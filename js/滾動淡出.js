c9d26710fe7aa9af75ba43f52f10f9051fbdeed0
立瑜 <baredragon0@gmail.com>
2024-03-22 09:46:13 +0800

    Update 滾動淡出.js

--- js/滾動淡出.js
+++ js/滾動淡出.js
@@ -11,15 +11,14 @@ window.addEventListener('scroll', function() {
 });
 
 
-//滑鼠滾動一整個頁面
+//滑鼠滾動一整個頁面  //解決google chrome瀏覽器不能的問題+空白鍵
 
 const navLinks = document.querySelectorAll('[data-nav-link]');
 let currentSectionIndex = 0;
 
-// 找到目前所在的section
 function findCurrentSectionIndex() {
   let index = 0;
-  for (let i = 0; i < navLinks.length+1; i++) {
+  for (let i = 0; i < navLinks.length; i++) {
     const id = navLinks[i].getAttribute('href').slice(1);
     const section = document.getElementById(id);
     const rect = section.getBoundingClientRect();
@@ -45,3 +44,52 @@ window.addEventListener('wheel', (e) => {
   const section = document.getElementById(id);
   section.scrollIntoView({ behavior: 'smooth' });
 });
+
+window.addEventListener('keydown', (e) => {
+  if (e.code === 'Space') {
+    e.preventDefault();
+    if (currentSectionIndex === navLinks.length - 1) {
+      currentSectionIndex = 0;
+    } else {
+      currentSectionIndex = Math.min(currentSectionIndex + 1, navLinks.length - 1);
+    }
+    const id = navLinks[currentSectionIndex].getAttribute('href').slice(1);
+    const section = document.getElementById(id);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+
+
+// const navLinks = document.querySelectorAll('[data-nav-link]');
+// let currentSectionIndex = 0;
+
+// // 找到目前所在的section
+// function findCurrentSectionIndex() {
+//   let index = 0;
+//   for (let i = 0; i < navLinks.length+1; i++) {
+//     const id = navLinks[i].getAttribute('href').slice(1);
+//     const section = document.getElementById(id);
+//     const rect = section.getBoundingClientRect();
+//     if (rect.top >= 0 && rect.top <= window.innerHeight) {
+//       index = i;
+//     }
+//   }
+//   return index;
+// }
+
+// window.addEventListener('wheel', (e) => {
+//   e.preventDefault();
+  
+//   const direction = e.deltaY > 0 ? 'down' : 'up';
+
+//   if (direction === 'down') {
+//     currentSectionIndex = Math.min(currentSectionIndex + 1, navLinks.length - 1);
+//   } else {
+//     currentSectionIndex = Math.max(currentSectionIndex - 1, 0);
+//   }
+
+//   const id = navLinks[currentSectionIndex].getAttribute('href').slice(1);
+//   const section = document.getElementById(id);
+//   section.scrollIntoView({ behavior: 'smooth' });
+// });
